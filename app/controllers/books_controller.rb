214d6949bdb8193ead575dda29f167f58a0c9f11
Seiryu-alt <seiryusain@gmail.com>
2021-04-08 06:01:29 +0000

    [add] bookモデルにバリデーションを実装、create失敗時にエラーメッセージを表示するように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,29 +3,33 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @books = Book.all
+      render :index
     end
-    redirect_to book_path(book)
   end
 
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
       flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
     end
-    redirect_to book_path(book)
   end
 
   def destroy
