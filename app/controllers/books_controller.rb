0859e4ce58f89cf4e210d54fdeacfd65c1e5449c
Seiryu-alt <seiryusain@gmail.com>
2021-04-08 03:53:57 +0000

    [add] updateアクション成功時にサクセスメッセージを表示するように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,9 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    end
     redirect_to book_path(book)
   end
 
