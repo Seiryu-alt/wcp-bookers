1cc49db90381b8f279d813508ee2a290660cfd56
Seiryu-alt <seiryusain@gmail.com>
2021-04-08 03:27:17 +0000

    [add] Gem: pry-byebug追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,19 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book)
-  end
-
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def edit
-    @book = Book.find(params[:id])
   end
 
   def update
